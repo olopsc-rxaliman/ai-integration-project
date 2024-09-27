152d3895ae7e14cfbc0957fbc9b039377d0a9f1f
olopsc-rxaliman <raliman22a-0077@olopsc.edu.ph>
2024-09-27 16:29:27 +0800

    Completed Launch Page (App Icon is still placeholder)

--- lib/pages/launch_page.dart
+++ lib/pages/launch_page.dart
@@ -10,22 +10,58 @@ class LaunchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
       backgroundColor: Colors.white,
-      splash: const Row(
+      splash: Row(
         mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(
-            "Splash Screen",
-            style: TextStyle(
-              color: Colors.lightBlue,
-              fontWeight: FontWeight.bold,
-              fontSize: 25,
-            ),
+          Image.asset(
+            'lib/resources/images/gemini.png',
+            scale: 3,
+          ),
+          const SizedBox(width: 20),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "AI Learn",
+                style: TextStyle(
+                  fontFamily: 'LeagueSpartan',
+                  color: Colors.lightBlue,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Powered by: ",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                    ),
+                  ),
+                  const Text(
+                    "Google Gemini ",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Image.asset(
+                    'lib/resources/images/gemini.png',
+                    scale: 17,
+                    isAntiAlias: true,
+                  ),
+                ],
+              ),
+            ],
           ),
         ],
       ),
       animationDuration: const Duration(seconds: 1),
-      duration: 2000,
+      duration: 2500,
       nextScreen: const HomePage(),
       splashTransition: SplashTransition.fadeTransition,
       pageTransitionType: PageTransitionType.fade,
